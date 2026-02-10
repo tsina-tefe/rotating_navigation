c69d9fdfff8ca0ede98835873acd1356e83b8faf
tsina-tefe <tsinukaltefera34@gmail.com>
2026-02-10 14:46:40 +0300

    Add special classes to show and hide the nav

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 document.querySelector('.open').addEventListener('click', () => {
-    document.querySelector('.circle').classList.add('rotate');
+    document.querySelector('.circle').classList.add('rotate-button');
+    document.querySelector('.page').classList.add('rotate-page');
+    document.querySelector('.nav-hidden').classList.add('rotate-nav');
 });
 
 document.querySelector('.close').addEventListener('click', () => {
-    document.querySelector('.circle').classList.remove('rotate');
+    document.querySelector('.circle').classList.remove('rotate-button');
+    document.querySelector('.page').classList.remove('rotate-page');
+    document.querySelector('.nav-hidden').classList.remove('rotate-nav');
 });
 
